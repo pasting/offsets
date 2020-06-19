1ff9a039c84f5cd0677c782e067f1083784c1d0c
pasting <67171342+pasting@users.noreply.github.com>
2020-06-19 21:45:01 +0000

    offsets

---
+++ offsets.cc
@@ -0,0 +1,37 @@
+UWorld : 0x8af58e0
+LineOfSight : 0x4730ab0
+Free : 0x30612b0
+GetObjName : 0x4882b00
+GObjects : 0x89c17f8
+GetObjNameIndex : 0x31646b0
+OwningGameInstance : 0x190
+Levels : 0x148
+LocalPlayers : 0x38
+ControlRotation : 0x288
+AcknowledgedPawn : 0x2a0
+PlayerState : 0x240
+RootComponent : 0x130
+CustomTimeDilation : 0x98
+Mesh : 0x280
+RelativeLocation : 0x11c
+ComponentVelocity : 0x140
+StaticMesh : 0x478
+CachedWorldSpaceBounds : 0x5f8
+bIsDBNO : 0x552
+bIsDying : 0x538
+CurrentWeapon : 0x5a0
+PrimaryPickupItemEntry : 0x298
+ItemDefinition : 0x18
+DisplayName : 0x70
+Tier : 0x54
+TeamIndex : 0xe68
+WeaponData : 0x360
+AmmoCount : 0x91c
+WeaponStatHandle : 0x7b8
+FireStartLoc : 0x880
+ReloadTime : 0xfc
+bAlreadySearched : 0xc69
+GetBounds_Addr: 0x1e3a8f1
+GetBoneMatrix_Addr: 0x49d6a95
+ValidateFire_Addr: 0x26ec211
+CallRespawn_Addr: 0x3fe8969
